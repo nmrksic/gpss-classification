deadb1332a1f9cdf51e40cace93d3af4c767b050
David Duvenaud <dkd23@cam.ac.uk>
2014-04-07 12:31:07 +0100

    Beginning cleanup

--- code/InitialiseRand.m
+++
@@ -1,9 +0,0 @@
-function InitialiseRand( seed )
-%INITIALISERAND Seeds rand and randn
-% Not really sure about differences between twister and state
-% If you do, please comment / change
-%
-% James Lloyd, June 2012
-  rand('twister', seed);
-  randn('state', seed);
-end
--- code/calculateAcc.m
+++
@@ -1,9 +0,0 @@
-function [accuracy] = calculateAcc(lp, X, y)
-
- predictions = pullClasses(exp(lp), 0.5); % turn probabilities into +-1 predictions
-
- errors = sum ( abs(predictions' - y) ) / 2; % number of prediction errors made
- 
- accuracy = 1 - errors / size(X, 1);
-
-end
