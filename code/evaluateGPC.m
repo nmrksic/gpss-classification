dbe32dcebcde42d5a0df7246b67668e1e4864f4a
Nikola Mrksic <nm480@montevideo.eng.cam.ac.uk>
2014-10-22 11:28:51 +0100

    merge all

--- code/evaluateGPC.m
+++ code/evaluateGPC.m
@@ -68,7 +68,7 @@ end
     
     if use_fixed_folds == 1
         for folds = 1:10
-            name_to_load = ['dave_data/classification_', expName, '_fold_', num2str(folds), '_of_10.mat'];
+            name_to_load = ['dave_data/classification_', expName, '_fold_', num2str(folds), '_of_10.mat']
             load(name_to_load)
             trnX{folds} = X;
             trnY{folds} = y;
